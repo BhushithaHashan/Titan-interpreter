d0cd38fb7f193089595acbd8081fc2ec950bebe6
BhushithaHashan <bhushithahashan99@gmail.com>
2025-08-04 09:13:14 +0530

    node recursion

--- jit.c
+++ jit.c
@@ -8,6 +8,7 @@
 #include <limits.h>
 struct Node{
     int val;
+    int op;
     struct Node *left;
     struct Node *right;
 };
@@ -29,13 +30,14 @@ int sum(struct Node *n){
 int find_max(struct Node *n){
     if (n==NULL)
     {
-        return;
+        return INT_MIN;
     }
     int max_lft = find_max(n->left);
     int max_right  = find_max(n->right);
     int max= (n->val>max_lft)?n->val:(max_lft>max_right)?max_lft:max_right;
     return max;
 }
+
 unsigned char code[] = { 0x8d, 0x04, 0x37, 0xc3 };
 int fac(int n){
     if (n<0)
@@ -48,6 +50,28 @@ int fac(int n){
         return 1;
     }
     return n*fac(n-1);
+}
+int add(int a,int b){
+    return a+b;
+}
+static int (*op[])(int,int)={add};
+void traverse(struct Node *n,int *ptr){
+    if (n->right==NULL)
+    {
+        return;
+    }
+    if (n->op==0)
+    {
+        traverse(n->right,ptr);
+    }
+    if (n->op==1)
+    {
+        int result = op[0](n->left->val,n->right->val);
+        *ptr = result;
+        return;
+    }
+    
+    
 }
 int main(){
     void *mem = (void *)syscall(SYS_mmap,NULL,sizeof(char)*4,PROT_READ|PROT_WRITE,MAP_ANONYMOUS|MAP_PRIVATE,-1,0);
@@ -66,13 +90,23 @@ int main(){
     int val = add(5,5);
     printf("%d\n",val);
 
-    printf("%d",fac(5));
-    if (5>NULL) 
-    {
-        printf("hello\n");
-    }
+    printf("%d\n",fac(5));
     
-    
+    struct Node op,left,right;
+    op.op = 1;
+    left.val = 5;
+    right.val = 10;
+    left.op = 0;
+    right.op = 0;
+    op.right = &right;
+    op.left = &left;
+    left.right= &op;
+    left.left = NULL;
+    right.left = &op;
+    right.right = NULL;
+    int result = 0;
+    traverse(&left,&result);
+    printf("%d\n",result);
 
 }
 
