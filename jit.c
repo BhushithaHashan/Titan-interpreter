6c5852c52b13ac0bccfa82b3747831fa5c40772e
BhushithaHashan <bhushithahashan99@gmail.com>
2025-08-04 06:12:44 +0530

    mem permission change

--- jit.c
+++ jit.c
@@ -3,11 +3,25 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <stddef.h>
+#include <string.h>
 #include <sys/syscall.h>
 
 unsigned char code[] = { 0x8d, 0x04, 0x37, 0xc3 };
 int main(){
-    void *mem = syscall(SYS_mmap,NULL,sizeof(char)*4,PROT_READ|PROT_WRITE,MAP_ANONYMOUS|MAP_PRIVATE,-1,0);
+    void *mem = (void *)syscall(SYS_mmap,NULL,sizeof(char)*4,PROT_READ|PROT_WRITE,MAP_ANONYMOUS|MAP_PRIVATE,-1,0);
+    if (mem == (void *)-1)
+    {
+        perror("mem alloc failed\n");
+        return -1;
+    }
+    memcpy(mem,code,sizeof(char)*4);
+    int stat = syscall(SYS_mprotect,mem,sizeof(char)*4,PROT_READ|PROT_EXEC);
+    if (stat!=-1)
+    {
+        printf("permission on mem changed\n");
+    }
     
+    
+
 }
 
