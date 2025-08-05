657e6e232511f98e769f7f72b416b99192105b51
BhushithaHashan <bhushithahashan99@gmail.com>
2025-08-05 08:49:15 +0530

    Lexer
    -define lexer functions
    
    -def read_number for lexer

--- titan/src/lexer.c
+++ titan/src/lexer.c
@@ -1,7 +1,13 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <ctype.h>
 #include "token.h"
 #include "lexer.h"
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <stdlib.h>
 
 void lexer_init(Lexer *lexer,char *src){
     lexer->src = src;
@@ -45,4 +51,55 @@ static void skip_white_spaces(Lexer *lexer){
     }
     
 }
+static char peek(Lexer *lexer){
+    if (lexer->src[lexer->pos+1]=='\0')
+    {
+        return '\0';
+    }
+    return lexer->src[lexer->pos+1];
+
+}
+static Token read_number(Lexer *lexer){
+    int number = 0;
+    Token token;
+    token.type = TOKEN_NUMBER;
+    const char *start = lexer->src+lexer->pos;
+    char *numberstr;
+    int count = 0;
+    while (1)
+    {
+        if (isdigit(lexer->src[lexer->pos]))
+        {
+            count++;
+            advance(lexer);
+        }else{     
+            break;
+        } 
+    }
+    void *mem =(void *)syscall(SYS_mmap,NULL,sizeof(char)*count+1,PROT_READ|PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS,-1,0);
+    if (mem== (void *)-1)
+    {   
+        perror("mmap failed\n");
+        exit(1);
+    }
+    numberstr = mem;
+    memcpy(numberstr,start,count);
+    numberstr[count] = '\0';
+    token.lexeme = numberstr;
+    for (size_t i = 0; i < count; i++)
+    {
+        int digit =numberstr[i]-48;
+        number = number * 10 + digit;  
+    }
+    token.value = number;
+    return token;
+}
 Token lexer_next_token(Lexer *lexer);
+
+int main(){
+    char *src = "1234";
+    Lexer lexer;
+    lexer_init(&lexer,src);
+    Token token = read_number(&lexer);
+    printf("lexeme:%s\tvalue:%d\n",token.lexeme,token.value);
+}
