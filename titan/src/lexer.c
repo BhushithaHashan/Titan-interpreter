fe9a1332ba8c2bb6c1657d18ee3528e05d3d74a5
BhushithaHashan <bhushithahashan99@gmail.com>
2025-08-05 09:38:11 +0530

    Lexer
    -define lexer functions
    
    -def read_identifiers for lexer

--- titan/src/lexer.c
+++ titan/src/lexer.c
@@ -94,12 +94,78 @@ static Token read_number(Lexer *lexer){
     token.value = number;
     return token;
 }
+static Token read_identifier(Lexer *lexer){
+    Token token;
+    const char *start = lexer->src+lexer->pos;
+    int len = 0;
+    while (isalnum(lexer->current_pointed_char)||lexer->current_pointed_char=='_')
+    {
+        len++;
+        advance(lexer);
+    }
+    printf("len:%d\n",len);
+    void *mem = (void *)syscall(SYS_mmap,NULL,sizeof(char)*(len+1),PROT_READ|PROT_WRITE,MAP_ANONYMOUS|MAP_PRIVATE,-1,0);
+    if (mem==(void *)-1)
+    {
+        perror("mmap error\n");
+        exit(1);
+    }
+    char *identifier = mem;
+    memcpy(identifier,start,len);
+    identifier[len] = '\0';
+    printf("%s\n",identifier);
+    switch (identifier[0]) {
+        case 'l':
+            if (strcmp(identifier, "let") == 0){
+                token.lexeme = identifier;
+                token.type = TOKEN_LET;
+                return token;
+            }   
+            break;
+        case 'i':
+            if (strcmp(identifier, "if") == 0){
+                token.lexeme = identifier;
+                token.type = TOKEN_IF;
+                return token;
+            }
+                
+            break;
+
+        // case 'w':
+        //     if (strcmp(identifier, "while") == 0)
+        //         return TOKEN_WHILE;
+        //     break;
+
+        // case 'f':
+        //     if (strcmp(identifier, "function") == 0)
+        //         return TOKEN_FUNCTION;
+        //     break;
+
+        // case 'r':
+        //     if (strcmp(identifier, "return") == 0)
+        //         return TOKEN_RETURN;
+        //     break;
+
+        // case 'p':
+        //     if (strcmp(identifier, "print") == 0)
+        //         return TOKEN_PRINT;
+        //     break;
+        default:
+            token.lexeme = identifier;
+            token.type = TOKEN_IDENTIFIER;
+            return token;
+            break;
+    }
+}
 Token lexer_next_token(Lexer *lexer);
 
 int main(){
     char *src = "1234";
+    char *idensrc ="if";
     Lexer lexer;
-    lexer_init(&lexer,src);
-    Token token = read_number(&lexer);
-    printf("lexeme:%s\tvalue:%d\n",token.lexeme,token.value);
+    lexer_init(&lexer,idensrc);
+    Token tokeniden = read_identifier(&lexer);
+    // Token token = read_number(&lexer);
+    // printf("lexeme:%s\tvalue:%d\n",token.lexeme,token.value);
+    printf("lexeme:%s\tType:%d\n",tokeniden.lexeme,tokeniden.type);
 }
